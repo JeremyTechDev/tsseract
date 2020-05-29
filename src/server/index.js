b84fa23b0a250843729e637483a3b62f7d1f68ed
Jeremy <jermy2918@gmail.com>
2020-05-29 09:12:18 -0400

    create server and mongo connection

---
+++ src/server/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose
+  .connect(`mongodb://localhost/${process.env.DB_NAME}`)
+  .then(() => console.log('📡 Connected to MongoDB...'))
+  .catch((error) => console.log('Error connections to MongoDB', error.message));
+
+app.get('/', (req, res) => res.send('Tsseract App'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Running server on port ${PORT}...`));
