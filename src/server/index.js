14e5fe35723c4b5b0cda5d1fcecc495ae65b7da1
Jeremy <jermy2918@gmail.com>
2020-05-31 19:18:53 -0400

    create user endpoints

--- src/server/index.js
+++ src/server/index.js
@@ -1,18 +1,22 @@
-require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const { DB_NAME, NODE_ENV } = require('../config/env');
+
+const user = require('./routes/user');
 
 const app = express();
 
 mongoose
-  .connect(`mongodb://localhost/${process.env.DB_NAME}`, {
+  .connect(`mongodb://localhost/${DB_NAME}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => console.log('📡 Connected to MongoDB...'))
   .catch((error) => console.log('Error connections to MongoDB', error.message));
 
+app.use(express.json());
 app.get('/', (req, res) => res.send('Tsseract App'));
+app.use('/api/users', user);
 
-const PORT = process.env.NODE_ENV || 5000;
+const PORT = NODE_ENV || 5000;
 app.listen(PORT, () => console.log(`🚀 Running server on port ${PORT}...`));
