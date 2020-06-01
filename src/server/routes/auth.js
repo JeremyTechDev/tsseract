2727f8f56c19f1dc03dff825ac1ef6ea826ac9fd
Jeremy <jermy2918@gmail.com>
2020-05-31 22:21:41 -0400

    create auth middleware

---
+++ src/server/routes/auth.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const authControllers = require('../controllers/auth');
+
+/**
+ * Creates a new user
+ * @route /api/auth/
+ */
+router.post('/', authControllers.auth);
+
+module.exports = router;
