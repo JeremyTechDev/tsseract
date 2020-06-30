8adbf8604e516712da11b074894a0d312d714469
Jeremy <jeremy2918@gmail.com>
2020-06-30 15:56:43 -0400

    Improved security to avoid auth with unexisting users

--- src/server/middlewares/authorization.js
+++ src/server/middlewares/authorization.js
@@ -1,13 +1,19 @@
+const { User } = require('../models/user');
+
 /**
  * Ensures that the user performing the action coincides with the credentials
  * @param req Express request object
  * @param res Express response object
  * @param next Next middleware function
  */
-exports.userAuth = (req, res, next) => {
+exports.userAuth = async (req, res, next) => {
   try {
     const token = req.user;
     const userId = req.body.user ? req.body.user : req.params.id;
+
+    const userExists = await User.findById(userId);
+    if (!userExists)
+      return res.status(404).send({ message: 'Access denied. Invalid User' });
 
     if (token.id !== userId) {
       return res.status(403).send({
