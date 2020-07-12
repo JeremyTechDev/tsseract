4c635167ef499bb6b77cf7d62062f813d3942f6a
Jeremy Muñoz Torres <jeremy2918@gmail.com>
2020-07-12 13:44:00 -0400

    Implement manifest (#12)
    * Add manifest
    
    * Enhance server security
    
    * Set .env.sample file

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -3,18 +3,57 @@ import Document, { Head, Main, NextScript } from 'next/document';
 export default class extends Document {
   render() {
     return (
-      <html lang="en">
+      <html lang="en-US">
         <Head>
           <meta charSet="UTF-8" />
+          <link rel="manifest" href="/static/manifest.json" />
+
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="application-name" content="Tsseract" />
+          <meta name="apple-mobile-web-app-title" content="Tsseract" />
+          <meta name="theme-color" content="#f6404f" />
+          <meta name="msapplication-navbutton-color" content="#f6404f" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="black-translucent"
+          />
+          <meta name="msapplication-starturl" content="/" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="512x512"
+            href="/static/icons/icon-512x512.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            type="image/png"
+            sizes="512x512"
+            href="/static/icons/icon-512x512.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="192x192"
+            href="/static/icons/icon-192x192.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            type="image/png"
+            sizes="192x192"
+            href="/static/icons/icon-192x192.png"
+          />
+
           <meta
             name="description"
             content="Tsseract in a social media app were can create content and earn money 💸 with it."
           />
           <meta name="author" content="Jeremy Muñoz" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
         </Head>
         <body>
           <Main />
