f16bacb41cebefa04d5638accce7905e871876f2
Jeremy <jeremy2918@gmail.com>
2020-08-27 17:30:41 -0400

    Created UploadImage component

--- client/components/CreatePost/styles.ts
+++ client/components/CreatePost/styles.ts
@@ -1,27 +1,23 @@
 import { makeStyles } from '@material-ui/core';
 
-const useStyles = makeStyles({
-  margin: {
-    margin: 10,
-  },
-  padding: {
-    padding: 10,
-  },
+const useStyles = makeStyles((theme) => ({
+  margin: { margin: 10 },
+  padding: { padding: 10 },
   titleTextArea: {
     background: 'none',
     border: 'none',
-    color: '#fff',
     fontFamily: 'Playfair Display',
     fontSize: 35,
     fontWeight: 500,
     margin: '10px 10px 0',
     resize: 'none',
     width: 'calc(100% - 15px)',
+    color: theme.palette.type === 'dark' ? '#fff' : '#000',
   },
   bodyTextArea: {
     background: 'none',
     border: 'none',
-    color: '#fff',
+    color: theme.palette.type === 'dark' ? '#fff' : '#000',
     fontSize: 20,
     fontWeight: 500,
     margin: '10px',
@@ -29,18 +25,17 @@ const useStyles = makeStyles({
     width: 'calc(100% - 15px)',
   },
   coverImg: {
-    display: 'block',
-    margin: '5px auto',
-    width: '100%',
-    height: '50vh',
-    objectFit: 'cover',
     borderRadius: 4,
+    display: 'block',
+    height: '50vh',
+    margin: '5px auto',
+    objectFit: 'cover',
+    width: '100%',
   },
   currImgInput: {
-    width: '65%',
-    border: 'none',
-    padding: 5,
+    margin: 10,
+    [theme.breakpoints.down('md')]: {},
   },
-});
+}));
 
 export default useStyles;
