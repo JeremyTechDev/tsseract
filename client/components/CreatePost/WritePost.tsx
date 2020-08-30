fc7d7b57610a8da7e320700c207c303483e4d279
Jeremy Munoz Torres <jeremy2918@gmail.com>
2020-08-30 12:25:35 -0400

    Added highlight.js and created WritePost and PreviewPost components

---
+++ client/components/CreatePost/WritePost.tsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import {
+  Button,
+  Grid,
+  Paper,
+  TextareaAutosize,
+  Tooltip,
+  Typography,
+} from '@material-ui/core';
+
+import UploadImage from './UploadImg';
+import CoverImgModal from '../CoverImgModal';
+
+import useStyles from './styles';
+
+type InputChangeEvent = React.ChangeEvent<
+  HTMLInputElement | HTMLTextAreaElement
+>;
+
+interface Props {
+  children?: React.ReactNode;
+  coverImg: string;
+  handleChange: (event: InputChangeEvent) => void;
+  post: { title: string; content: string };
+  setCoverImg: React.Dispatch<React.SetStateAction<string>>;
+  setShowCoverImg: React.Dispatch<React.SetStateAction<boolean>>;
+  showCoverImg: boolean;
+}
+
+const WritePost: React.FC<Props> = ({
+  coverImg,
+  handleChange,
+  post,
+  setCoverImg,
+  setShowCoverImg,
+  showCoverImg,
+}) => {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <Grid justify="space-between" container>
+        {(coverImg && (
+          <Tooltip placement="top" title="Click to change">
+            <img
+              alt="Cover Image"
+              className={classes.coverImg}
+              src={coverImg}
+              onClick={() => setShowCoverImg(true)}
+            />
+          </Tooltip>
+        )) || (
+          <Button
+            className={classes.margin}
+            color="primary"
+            variant="outlined"
+            onClick={() => setShowCoverImg(true)}
+          >
+            Cover image
+          </Button>
+        )}
+        <Typography variant="caption">{125 - post.title.length}</Typography>
+      </Grid>
+
+      <TextareaAutosize
+        name="title"
+        className={classes.titleTextArea}
+        defaultValue={post.title}
+        maxLength={125}
+        onChange={handleChange}
+        placeholder="Add your post title here..."
+      />
+
+      <Typography className={classes.margin} variant="subtitle1">
+        Add up to 5 tags...
+      </Typography>
+
+      <Paper elevation={3}>
+        <UploadImage />
+      </Paper>
+
+      <TextareaAutosize
+        name="content"
+        className={classes.bodyTextArea}
+        defaultValue={post.content}
+        onChange={handleChange}
+        placeholder="Write you post content here..."
+        rowsMin={10}
+      />
+
+      <Typography variant="caption" align="right">
+        Markdown supported
+      </Typography>
+
+      {showCoverImg && (
+        <CoverImgModal
+          img={coverImg}
+          open={setShowCoverImg}
+          setImg={setCoverImg}
+        />
+      )}
+    </React.Fragment>
+  );
+};
+
+export default WritePost;
--- client/helpers/markDown.ts
+++ client/helpers/markDown.ts
@@ -1,6 +1,15 @@
-import emoji from 'node-emoji';
-import marked from 'marked';
 import dompurify from 'dompurify';
+import emoji from 'node-emoji';
+import hljs from 'highlight.js';
+import marked from 'marked';
+
+marked.options({
+  gfm: true,
+  highlight: function (code, lang) {
+    const validLang = hljs.getLanguage(lang) ? lang : 'plaintext';
+    return hljs.highlight(validLang, code).value;
+  },
+});
 
 /**
  * Returns a dangerouslySetInnerHTML with a clean markdown string
