827fa1461a0d798a7eecfbad9ab2518cadb2578b
Jeremy Muñoz Torres <jeremy2918@gmail.com>
2020-11-15 19:45:57 -0400

    Removed unnecessary useEffect and replaced them with getInitialProps (#41)

--- client/components/Login/index.tsx
+++ client/components/Login/index.tsx
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import {
   Button,
-  CircularProgress,
   Container,
   Divider,
   Grid,
@@ -12,14 +11,16 @@ import {
 import ImgInfo from './ImgInfo';
 import SignIn from './SignIn';
 import SignUp from './SignUp';
-import getRandomImg from '../../helpers/getRandomImg';
 import useForm from '../../hooks/useForm';
 import useStyles from './styles';
 import { iBackgroundImageData } from '../../@types';
 
-const Login = () => {
-  const [bgData, setBgData] = useState<iBackgroundImageData>({ img: '' });
-  const [loading, setLoading] = useState(false);
+interface Props {
+  bgData: iBackgroundImageData;
+}
+
+const Login: React.FC<Props> = ({ bgData }) => {
+  const classes = useStyles({ bg: bgData.img });
   const [showSignUp, setShowSignUp] = useState(false);
   const [user, handleChange] = useForm({
     birthDate: '',
@@ -29,63 +30,44 @@ const Login = () => {
     rPassword: '',
     username: '',
   });
-  const classes = useStyles({ bg: bgData.img });
-
-  useEffect(() => {
-    setLoading(true);
-    const fetchBgData = async () => {
-      setBgData(await getRandomImg());
-      setLoading(false);
-    };
-
-    fetchBgData();
-  }, []);
-
-  const handleViewChange = () => {
-    setShowSignUp(!showSignUp);
-  };
 
   return (
-    (loading && (
-      <CircularProgress size={100} className={classes.centered} />
-    )) || (
-      <Grid className={classes.grid} container direction="row">
-        <Hidden smDown>
-          <Grid container item md={2} alignItems="flex-end">
-            <ImgInfo bgData={bgData} />
-          </Grid>
-        </Hidden>
-
-        <Grid item xs={12} md={5} />
-
-        <Grid container item xs={false} md={4} alignItems="center">
-          <Container className={classes.imgInfo}>
-            {(showSignUp && (
-              <SignUp user={user} handleChange={handleChange} />
-            )) || <SignIn user={user} handleChange={handleChange} />}
-
-            <Divider light />
-
-            <Typography align="center">
-              {showSignUp
-                ? 'Already have an account?'
-                : "Don't have an account yet?"}
-              <Button color="primary" onClick={handleViewChange}>
-                {showSignUp ? 'Sign In' : 'Sign Up'}
-              </Button>
-            </Typography>
-          </Container>
+    <Grid className={classes.grid} container direction="row">
+      <Hidden smDown>
+        <Grid container item md={2} alignItems="flex-end">
+          <ImgInfo bgData={bgData} />
         </Grid>
+      </Hidden>
 
-        <Hidden mdUp>
-          <Grid container item md={2} alignItems="flex-end">
-            <ImgInfo bgData={bgData} />
-          </Grid>
-        </Hidden>
+      <Grid item xs={12} md={5} />
 
-        <Grid item xs={1} md={1} />
+      <Grid container item xs={false} md={4} alignItems="center">
+        <Container className={classes.imgInfo}>
+          {(showSignUp && (
+            <SignUp user={user} handleChange={handleChange} />
+          )) || <SignIn user={user} handleChange={handleChange} />}
+
+          <Divider light />
+
+          <Typography align="center">
+            {showSignUp
+              ? 'Already have an account?'
+              : "Don't have an account yet?"}
+            <Button color="primary" onClick={() => setShowSignUp(!showSignUp)}>
+              {showSignUp ? 'Sign In' : 'Sign Up'}
+            </Button>
+          </Typography>
+        </Container>
       </Grid>
-    )
+
+      <Hidden mdUp>
+        <Grid container item md={2} alignItems="flex-end">
+          <ImgInfo bgData={bgData} />
+        </Grid>
+      </Hidden>
+
+      <Grid item xs={1} md={1} />
+    </Grid>
   );
 };
 
--- client/pages/login.tsx
+++ client/pages/login.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import { NextPage } from 'next';
 
-import Layout from '../components/Layout';
 import Login from '../components/Login';
+import getRandomImg from '../helpers/getRandomImg';
+import { iBackgroundImageData } from '../@types';
 
-const LoginPage: React.FC = () => {
-  return (
-    <Layout title="Tsseract App">
-      <Login />
-    </Layout>
-  );
+interface Props {
+  bgData: iBackgroundImageData;
+}
+
+const LoginPage: NextPage<Props> = ({ bgData }) => {
+  return <Login bgData={bgData} />;
 };
+
+LoginPage.getInitialProps = async () => {
+  const bgData = await getRandomImg();
+
+  return { bgData };
+};
+
 export default LoginPage;
