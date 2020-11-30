8b08c9e5292a499a50277914a58407cb709bab52
Jeremy <jeremy2918@gmail.com>
2020-11-30 19:24:24 -0400

    Added tag and posts dummy data

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,4 @@
 import next from 'next';
-import dummy from 'mongoose-dummy';
 
 import server from './server';
 const { NODE_ENV } = process.env;
@@ -8,18 +7,5 @@ const dev = NODE_ENV !== 'production';
 
 const app = next({ dev, dir: './dist/client' });
 const appHandler = app.getRequestHandler();
-
-import UserModel from './models/user';
-
-const result = [];
-for (let index = 0; index < 20; index++) {
-  const x = dummy(UserModel, {
-    ignore: ['followers', 'following'],
-    returnDate: true,
-  });
-  result.push(x);
-}
-
-console.log(JSON.stringify(result));
 
 app.prepare().then(() => server({ dev, appHandler }));
--- server/seeders/index.ts
+++ server/seeders/index.ts
@@ -1,17 +1,39 @@
 import database from '../database';
 import UserModel from '../models/user';
+import TagModel from '../models/tag';
+import PostModel from '../models/post';
 
-// Dummy data from JSON
-import userDummyData from './users.json';
+import postsDummyData from './posts.json';
+import tagsDummyData from './tags.json';
+import usersDummyData from './users.json';
 
 database()
-  .then(() => {
+  .then(async () => {
     try {
-      userDummyData.forEach((dummyData) => new UserModel(dummyData).save());
+      console.info('🌱 Seeding database...');
+      await Promise.all(
+        postsDummyData.map(
+          async (dummyData) => await new PostModel(dummyData).save(),
+        ),
+      );
+      await Promise.all(
+        tagsDummyData.map(
+          async (dummyData) => await new TagModel(dummyData).save(),
+        ),
+      );
+      await Promise.all(
+        usersDummyData.map(
+          async (dummyData) => await new UserModel(dummyData).save(),
+        ),
+      );
+      console.info('✅ Seeding successful...');
     } catch (error) {
-      console.error('Something went wrong running seeders');
+      console.error('🛑 Something went wrong running seeders');
+    } finally {
+      process.exit(0);
     }
   })
-  .catch(() =>
-    console.error('Unable to connect to the database to run the seeders'),
-  );
+  .catch(() => {
+    console.error('🛑 Unable to connect to the database to run the seeders');
+    process.exit(0);
+  });
