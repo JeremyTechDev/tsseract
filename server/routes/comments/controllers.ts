f418f8f37c334f45c3dba13046b5ea25e9bd11df
Jeremy Muñoz Torres <JMunoz@contractor.nexstardigital.com>
2021-03-27 18:09:18 -0400

    Fixed server TS errors

--- server/routes/comments/controllers.ts
+++ server/routes/comments/controllers.ts
@@ -12,36 +12,36 @@ import { iComment, iPost } from '../../@types';
  * @param {Object} req.body User data
  */
 export const createComment: RequestHandler = async (req, res) => {
-  try {
-    const { postId } = req.params;
-    const { _id: userId } = req.cookies.profile;
+    try {
+        const { postId } = req.params;
+        const { _id: userId } = req.cookies.profile;
 
-    const { error } = validateComment(
-      Object.assign(req.body, { user: userId.toString() }),
-    );
-    if (error) return res.status(400).send({ error: error.details[0].message });
+        const { error } = validateComment(
+            Object.assign(req.body, { user: userId.toString() }),
+        );
+        if (error) return res.status(400).send({ error: error.details[0].message });
 
-    if (!mongoose.isValidObjectId(postId))
-      return res.status(404).send({ error: 'No post found with the given id' });
+        if (!mongoose.isValidObjectId(postId))
+            return res.status(404).send({ error: 'No post found with the given id' });
 
-    const post = (await Post.findByIdAndUpdate(
-      postId,
-      {
-        $push: { comments: { $each: [req.body], $position: 0 } },
-        $inc: { interactions: 1 },
-      },
-      { new: true },
-    )
-      .populate('user', '_id name username')
-      .populate('comments.user', '_id name username')) as iPost;
+        const post = (await Post.findByIdAndUpdate(
+            postId,
+            {
+                $push: { comments: { $each: [req.body], $position: 0 } },
+                $inc: { interactions: 1 },
+            },
+            { new: true },
+        )
+            .populate('user', '_id name username')
+            .populate('comments.user', '_id name username')) as iPost;
 
-    if (!post)
-      return res.status(404).send({ error: 'No post found with the given id' });
+        if (!post)
+            return res.status(404).send({ error: 'No post found with the given id' });
 
-    res.send(post);
-  } catch (error) {
-    return res.status(500).send({ error: error.message });
-  }
+        res.send(post);
+    } catch (error) {
+        return res.status(500).send({ error: error.message });
+    }
 };
 
 /**
@@ -51,29 +51,30 @@ export const createComment: RequestHandler = async (req, res) => {
  * @param {Object} req.params.commentId comment id
  */
 export const deleteComment: RequestHandler = async (req, res) => {
-  const { commentId } = req.params;
-  const { _id: userId } = req.cookies.profile;
+    const { commentId } = req.params;
+    const { _id: userId } = req.cookies.profile;
 
-  try {
-    const post = (await Post.findOne({ 'comments._id': commentId })) as iPost;
+    try {
+        const post = (await Post.findOne({ 'comments._id': commentId })) as iPost;
 
-    if (!post)
-      return res
-        .status(404)
-        .send({ error: 'No post found with the given comment id' });
+        if (!post)
+            return res
+                .status(404)
+                .send({ error: 'No post found with the given comment id' });
 
-    post.comments.forEach(async (comment: iComment) => {
-      if (comment.user.equals(userId) && comment._id.equals(commentId)) {
-        const newPost = (await Post.findByIdAndUpdate(
-          post._id,
-          { $pull: { comments: { _id: commentId } } },
-          { new: true },
-        )) as iPost;
+        post.comments.forEach(async (comment: iComment) => {
+            if (comment.user.equals(userId) && comment._id.equals(commentId)) {
+                const newPost = (await Post.findByIdAndUpdate(
+                    post._id,
+                    // @ts-ignore
+                    { $pull: { comments: { _id: commentId } } },
+                    { new: true },
+                )) as iPost;
 
-        return res.send(newPost);
-      }
-    });
-  } catch (error) {
-    return res.status(500).send({ error: error.message });
-  }
+                return res.send(newPost);
+            }
+        });
+    } catch (error) {
+        return res.status(500).send({ error: error.message });
+    }
 };
--- server/routes/users/controllers.ts
+++ server/routes/users/controllers.ts
@@ -9,20 +9,20 @@ import { iUser } from '../../@types';
 
 // to select all the user data but their password
 const SELECT =
-  '_id name username email birthDate createdAt followers following avatar';
+    '_id name username email birthDate createdAt followers following avatar';
 
 const output = (user: iUser) => {
-  return {
-    _id: user.id,
-    birthDate: user.birthDate,
-    createdAt: user.createdAt,
-    email: user.email,
-    followers: arrayToObj(user.followers, '_id'),
-    following: arrayToObj(user.following, '_id'),
-    name: user.name,
-    username: user.username,
-    avatar: user.avatar,
-  };
+    return {
+        _id: user.id,
+        birthDate: user.birthDate,
+        createdAt: user.createdAt,
+        email: user.email,
+        followers: arrayToObj(user.followers, '_id'),
+        following: arrayToObj(user.following, '_id'),
+        name: user.name,
+        username: user.username,
+        avatar: user.avatar,
+    };
 };
 
 /**
@@ -32,40 +32,40 @@ const output = (user: iUser) => {
  * @param {Object} req.body User data
  */
 export const createUser: RequestHandler = async (req, res) => {
-  try {
-    const { error } = validateUser(req.body);
-    if (error) return res.status(400).send({ error: error.details[0].message });
+    try {
+        const { error } = validateUser(req.body);
+        if (error) return res.status(400).send({ error: error.details[0].message });
 
-    const isUsernameTaken = await User.findOne({ username: req.body.username });
-    const isEmailTaken = await User.findOne({ email: req.body.email });
+        const isUsernameTaken = await User.findOne({ username: req.body.username });
+        const isEmailTaken = await User.findOne({ email: req.body.email });
 
-    if (isUsernameTaken)
-      return res.status(409).send({ error: 'Username already taken' });
-    if (isEmailTaken)
-      return res.status(409).send({ error: 'Email already taken' });
+        if (isUsernameTaken)
+            return res.status(409).send({ error: 'Username already taken' });
+        if (isEmailTaken)
+            return res.status(409).send({ error: 'Email already taken' });
 
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-    Object.assign(req.body, { password: hashedPassword });
+        Object.assign(req.body, { password: hashedPassword });
 
-    const user = new User({ ...req.body }) as iUser;
-    await user.save();
+        const user = new User({ ...req.body }) as iUser;
+        await user.save();
 
-    const userToken = {
-      _id: user._id,
-      email: user.email,
-      name: user.name,
-      username: user.username,
-    };
+        const userToken = {
+            _id: user._id,
+            email: user.email,
+            name: user.name,
+            username: user.username,
+        };
 
-    const { cookie, cookieConfig } = cookieCreator(userToken);
-    res.cookie('tsseract-auth-token', cookie, cookieConfig);
+        const { cookie, cookieConfig } = cookieCreator(userToken);
+        res.cookie('tsseract-auth-token', cookie, cookieConfig);
 
-    res.send(userToken);
-  } catch (error) {
-    return res.status(500).send({ error: error.message });
-  }
+        res.send(userToken);
+    } catch (error) {
+        return res.status(500).send({ error: error.message });
+    }
 };
 
 /**
@@ -75,21 +75,21 @@ export const createUser: RequestHandler = async (req, res) => {
  * @param {String} res.params.id User id
  */
 export const retrieveUser: RequestHandler = async (req, res) => {
-  try {
-    const userId = req.params.id;
+    try {
+        const userId = req.params.id;
 
-    const user = (await User.findById(userId)
-      .select(SELECT)
-      .populate('following', SELECT)
-      .populate('followers', SELECT)) as iUser;
+        const user = (await User.findById(userId)
+            .select(SELECT)
+            .populate('following', SELECT)
+            .populate('followers', SELECT)) as iUser;
 
-    if (!user)
-      return res.status(404).send({ error: 'No used found with the given id' });
+        if (!user)
+            return res.status(404).send({ error: 'No used found with the given id' });
 
-    res.send(output(user));
-  } catch (error) {
-    return res.status(500).send({ error: error.message });
-  }
+        res.send(output(user));
+    } catch (error) {
+        return res.status(500).send({ error: error.message });
+    }
 };
 
 /**
@@ -99,22 +99,22 @@ export const retrieveUser: RequestHandler = async (req, res) => {
  * @param {String} res.params.username User username
  */
 export const retrieveUserByUsername: RequestHandler = async (req, res) => {
-  try {
-    const { username } = req.params;
-    const user = (await User.findOne({ username })
-      .select(SELECT)
-      .populate('following', SELECT)
-      .populate('followers', SELECT)) as iUser;
+    try {
+        const { username } = req.params;
+        const user = (await User.findOne({ username })
+            .select(SELECT)
+            .populate('following', SELECT)
+            .populate('followers', SELECT)) as iUser;
 
-    if (!user)
-      return res
-        .status(404)
-        .send({ error: 'No user found with the given username' });
+        if (!user)
+            return res
+                .status(404)
+                .send({ error: 'No user found with the given username' });
 
-    res.send(output(user));
-  } catch (error) {
-    return res.status(500).send({ error: error.message });
-  }
+        res.send(output(user));
+    } catch (error) {
+        return res.status(500).send({ error: error.message });
+    }
 };
 
 /**
@@ -123,32 +123,32 @@ export const retrieveUserByUsername: RequestHandler = async (req, res) => {
  * @param {Object} res Express response
  */
 export const updateUser: RequestHandler = async (req, res) => {
-  const { _id: userId } = req.cookies.profile;
+    const { _id: userId } = req.cookies.profile;
 
-  const schema = Joi.object({
-    avatar: Joi.string().trim(),
-    name: Joi.string().trim().min(3).max(255),
-    email: Joi.string()
-      .email({ tlds: { allow: false } })
-      .min(2)
-      .max(255)
-      .trim(),
-  });
+    const schema = Joi.object({
+        avatar: Joi.string().trim(),
+        name: Joi.string().trim().min(3).max(255),
+        email: Joi.string()
+            .email({ tlds: { allow: false } })
+            .min(2)
+            .max(255)
+            .trim(),
+    });
 
-  const { error } = schema.validate(req.body);
-  if (error) return res.status(400).send({ error: error.details[0].message });
+    const { error } = schema.validate(req.body);
+    if (error) return res.status(400).send({ error: error.details[0].message });
 
-  try {
-    const user = (await User.findByIdAndUpdate(
-      userId,
-      { ...req.body },
-      { new: true },
-    )) as iUser;
+    try {
+        const user = (await User.findByIdAndUpdate(
+            userId,
+            { ...req.body },
+            { new: true },
+        )) as iUser;
 
-    return res.send(user);
-  } catch (error) {
-    return res.status(500).send({ error: error.message });
-  }
+        return res.send(user);
+    } catch (error) {
+        return res.status(500).send({ error: error.message });
+    }
 };
 
 /**
@@ -158,64 +158,66 @@ export const updateUser: RequestHandler = async (req, res) => {
  * @param {String} res.params.followToUsername user to toggle follow
  */
 export const toggleFollow: RequestHandler = async (req, res) => {
-  const { followToUsername } = req.params;
-  const followBy: iUser = req.cookies.profile;
+    const { followToUsername } = req.params;
+    const followBy: iUser = req.cookies.profile;
 
-  try {
-    const followTo = (await User.findOne({
-      username: followToUsername,
-    })) as iUser;
+    try {
+        const followTo = (await User.findOne({
+            username: followToUsername,
+        })) as iUser;
 
-    if (!followTo)
-      return res
-        .status(404)
-        .send({ error: 'No user found with the given username' });
+        if (!followTo)
+            return res
+                .status(404)
+                .send({ error: 'No user found with the given username' });
 
-    if (followTo._id.equals(followBy._id))
-      return res
-        .status(409)
-        .send({ error: 'You cannot follow your own account' });
+        if (followTo._id.equals(followBy._id))
+            return res
+                .status(409)
+                .send({ error: 'You cannot follow your own account' });
 
-    // If the auth user already follows the account, unfollow
-    let newFollowBy: iUser, newFollowTo: iUser, action: 'follow' | 'unfollow';
-    if (followBy.following.includes(followTo._id)) {
-      // Remove the followTo user from the followBy following list
-      newFollowBy = (await User.findOneAndUpdate(
-        { _id: followBy._id },
-        { $pull: { following: followTo._id } },
-        { new: true },
-      )) as iUser;
+        // If the auth user already follows the account, unfollow
+        let newFollowBy: iUser, newFollowTo: iUser, action: 'follow' | 'unfollow';
+        if (followBy.following.includes(followTo._id)) {
+            // Remove the followTo user from the followBy following list
+            newFollowBy = (await User.findOneAndUpdate(
+                { _id: followBy._id },
+                // @ts-ignore
+                { $pull: { following: followTo._id } },
+                { new: true },
+            )) as iUser;
 
-      // Remove the follower (followBy) from the followTo user
-      newFollowTo = (await User.findOneAndUpdate(
-        { _id: followTo._id },
-        { $pull: { followers: followBy._id } },
-        { new: true },
-      )) as iUser;
+            // Remove the follower (followBy) from the followTo user
+            newFollowTo = (await User.findOneAndUpdate(
+                { _id: followTo._id },
+                // @ts-ignore
+                { $pull: { followers: followBy._id } },
+                { new: true },
+            )) as iUser;
 
-      action = 'unfollow';
-    } else {
-      // Add the new following (followTo) to followBy user
-      newFollowBy = (await User.findOneAndUpdate(
-        { _id: followBy._id },
-        { $push: { following: followTo._id } },
-        { new: true },
-      )) as iUser;
+            action = 'unfollow';
+        } else {
+            // Add the new following (followTo) to followBy user
+            newFollowBy = (await User.findOneAndUpdate(
+                { _id: followBy._id },
+                { $push: { following: followTo._id } },
+                { new: true },
+            )) as iUser;
 
-      // Add the new follower (followBy) to the followTo user
-      newFollowTo = (await User.findOneAndUpdate(
-        { _id: followTo._id },
-        { $push: { followers: followBy._id } },
-        { new: true },
-      )) as iUser;
+            // Add the new follower (followBy) to the followTo user
+            newFollowTo = (await User.findOneAndUpdate(
+                { _id: followTo._id },
+                { $push: { followers: followBy._id } },
+                { new: true },
+            )) as iUser;
 
-      action = 'follow';
+            action = 'follow';
+        }
+
+        res.send({ following: newFollowBy, follower: newFollowTo, action });
+    } catch (error) {
+        return res.status(500).send({ error: error.message });
     }
-
-    res.send({ following: newFollowBy, follower: newFollowTo, action });
-  } catch (error) {
-    return res.status(500).send({ error: error.message });
-  }
 };
 
 /**
@@ -225,12 +227,12 @@ export const toggleFollow: RequestHandler = async (req, res) => {
  * @param {String} res.params.id User id
  */
 export const deleteUser: RequestHandler = async (req, res) => {
-  try {
-    const { _id: userId } = req.cookies.profile;
-    const user = await User.findByIdAndDelete(userId).select(SELECT);
+    try {
+        const { _id: userId } = req.cookies.profile;
+        const user = await User.findByIdAndDelete(userId).select(SELECT);
 
-    res.send(user);
-  } catch (error) {
-    return res.status(500).send({ error: error.message });
-  }
+        res.send(user);
+    } catch (error) {
+        return res.status(500).send({ error: error.message });
+    }
 };
