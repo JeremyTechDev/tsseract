2b28bd33917799d41f6688dc8bd9de59a758b065
Jeremy Muñoz Torres <JMunoz@contractor.nexstardigital.com>
2021-03-27 22:55:37 -0400

    Added Root GQL Queries

--- server/routes/posts/resolvers.ts
+++ server/routes/posts/resolvers.ts
@@ -1,9 +1,9 @@
 import {
-  GraphQLInt,
-  GraphQLList,
-  GraphQLNonNull,
-  GraphQLObjectType,
-  GraphQLString,
+    GraphQLInt,
+    GraphQLList,
+    GraphQLNonNull,
+    GraphQLObjectType,
+    GraphQLString,
 } from 'graphql';
 
 import Tag from '../tags/model';
@@ -14,35 +14,35 @@ import CommentType from '../comments/resolvers';
 import { iPost } from '../../@types';
 
 const PostType: GraphQLObjectType = new GraphQLObjectType({
-  name: 'Post',
-  description: 'Represents a post published by a user',
-  fields: () => ({
-    id: { type: GraphQLNonNull(GraphQLString) },
-    user: {
-      type: GraphQLNonNull(UserType),
-      description: 'The user who published the post',
-      resolve: async (parent: iPost) => await Users.findById(parent.user),
-    },
-    title: { type: GraphQLNonNull(GraphQLString) },
-    body: { type: GraphQLNonNull(GraphQLString) },
-    cover: { type: GraphQLString },
-    interactions: { type: GraphQLNonNull(GraphQLInt) },
-    likes: {
-      type: GraphQLList(UserType),
-      description: 'List of the users who have liked the post',
-      resolve: async (parent: iPost) =>
-        await parent.likes.map(async (user) => Users.findById(user)),
-    },
-    comments: { type: GraphQLList(CommentType) },
-    tags: {
-      type: GraphQLList(TagType),
-      description: 'List of the comments added to the post',
-      resolve: async (parent: iPost) =>
-        await parent.tags.map(async (tagId) => await Tag.findById(tagId)),
-    },
-    createdAt: { type: GraphQLNonNull(GraphQLString) },
-    updatedAt: { type: GraphQLNonNull(GraphQLString) },
-  }),
+    name: 'Post',
+    description: 'Represents a post published by a user',
+    fields: () => ({
+        id: { type: GraphQLNonNull(GraphQLString) },
+        user: {
+            type: GraphQLNonNull(UserType),
+            description: 'The user who published the post',
+            resolve: async (parent: iPost) => await Users.findById(parent.user),
+        },
+        title: { type: GraphQLNonNull(GraphQLString) },
+        body: { type: GraphQLNonNull(GraphQLString) },
+        cover: { type: GraphQLString },
+        interactions: { type: GraphQLNonNull(GraphQLInt) },
+        likes: {
+            type: GraphQLList(UserType),
+            description: 'List of the users who have liked the post',
+            resolve: (parent: iPost) =>
+                parent.likes.map(async (user) => Users.findById(user)),
+        },
+        comments: { type: GraphQLList(CommentType) },
+        tags: {
+            type: GraphQLList(TagType),
+            description: 'List of the comments added to the post',
+            resolve: (parent: iPost) =>
+                parent.tags.map(async (tagId) => await Tag.findById(tagId)),
+        },
+        createdAt: { type: GraphQLNonNull(GraphQLString) },
+        updatedAt: { type: GraphQLNonNull(GraphQLString) },
+    }),
 });
 
 export default PostType;
--- server/routes/tags/resolvers.ts
+++ server/routes/tags/resolvers.ts
@@ -1,18 +1,27 @@
 import {
-  GraphQLInt,
-  GraphQLNonNull,
-  GraphQLObjectType,
-  GraphQLString,
+    GraphQLInt,
+    GraphQLList,
+    GraphQLNonNull,
+    GraphQLObjectType,
+    GraphQLString,
 } from 'graphql';
 
+import PostType from '../posts/resolvers';
+import Posts from '../posts/model';
+
 const TagType: GraphQLObjectType = new GraphQLObjectType({
-  name: 'Tag',
-  description: 'Represents a tag that is assigned to a post',
-  fields: () => ({
-    id: { type: GraphQLNonNull(GraphQLString) },
-    name: { type: GraphQLNonNull(GraphQLString) },
-    popularity: { type: GraphQLNonNull(GraphQLInt) },
-  }),
+    name: 'Tag',
+    description: 'Represents a tag that is assigned to a post',
+    fields: () => ({
+        id: { type: GraphQLNonNull(GraphQLString) },
+        name: { type: GraphQLNonNull(GraphQLString) },
+        popularity: { type: GraphQLNonNull(GraphQLInt) },
+        posts: {
+            type: GraphQLList(PostType),
+            description: "List of posts with the tag",
+            resolve: async (parent) => await Posts.find({ tags: parent.id })
+        }
+    }),
 });
 
 export default TagType;
