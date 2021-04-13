817d5b5507e8c34161b91efaf4d26f80a99e77a3
Jeremy Muñoz Torres <JMunoz@contractor.nexstardigital.com>
2021-04-12 22:54:28 -0400

    Added Post Tests

--- server/routes/posts/mutations.ts
+++ server/routes/posts/mutations.ts
@@ -1,4 +1,11 @@
-import { Thunk, GraphQLFieldConfigMap, GraphQLNonNull, GraphQLString, GraphQLObjectType, GraphQLList } from 'graphql';
+import {
+	Thunk,
+	GraphQLFieldConfigMap,
+	GraphQLNonNull,
+	GraphQLString,
+	GraphQLObjectType,
+	GraphQLList,
+} from 'graphql';
 
 import Posts from './model';
 import Tags from '../tags/model';
@@ -7,104 +14,107 @@ import postValidator from './validator';
 import tagValidator from '../tags/validator';
 import { iPost, iTag, Context } from '../../@types';
 
-const Mutations: Thunk<GraphQLFieldConfigMap<any, any>> = ({
-    CreatePost: {
-        type: PostType,
-        description: 'Create a new post',
-        args: {
-            title: { type: GraphQLNonNull(GraphQLString) },
-            body: { type: GraphQLNonNull(GraphQLString) },
-            cover: { type: GraphQLString },
-            tags: { type: GraphQLList(GraphQLString) },
-        },
-        resolve: async (_, args, ctx: Context) => {
-            if (ctx.req.cookies.error) throw Error(ctx.req.cookies.errorMessage)
+const Mutations: Thunk<GraphQLFieldConfigMap<any, any>> = {
+	CreatePost: {
+		type: PostType,
+		description: 'Create a new post',
+		args: {
+			title: { type: GraphQLNonNull(GraphQLString) },
+			body: { type: GraphQLNonNull(GraphQLString) },
+			cover: { type: GraphQLString },
+			tags: { type: GraphQLList(GraphQLString) },
+		},
+		resolve: async (_, args, ctx: Context) => {
+			if (ctx.req.cookies.error) throw Error(ctx.req.cookies.errorMessage);
 
-            const { _id: userId } = ctx.req.cookies.profile;
-            const { error } = postValidator(
-                Object.assign(args, { user: userId.toString() }),
-            );
-            if (error) throw Error(error.details[0].message);
+			const { _id: userId } = ctx.req.cookies.profile;
+			const { error } = postValidator(
+				Object.assign(args, { user: userId.toString() }),
+			);
+			if (error) throw Error(error.details[0].message);
 
-            let createdTags: iTag[] = [];
-            if (ctx.req.body.tags && ctx.req.body.tags.length) {
-                const tagsError = tagValidator(ctx.req.body.tags);
+			let createdTags: iTag[] = [];
+			if (ctx.req.body.tags && ctx.req.body.tags.length) {
+				const tagsError = tagValidator(ctx.req.body.tags);
 
-                if (tagsError) throw Error('Invalid tag or tags');
+				if (tagsError) throw Error('Invalid tag or tags');
 
-                createdTags = <iTag[]>await Promise.all(
-                    ctx.req.body.tags.map(async (tagName: string) => {
-                        const tagExists = (await Tags.findOne({ name: tagName })) as iTag;
+				createdTags = <iTag[]>await Promise.all(
+					ctx.req.body.tags.map(async (tagName: string) => {
+						const tagExists = (await Tags.findOne({ name: tagName })) as iTag;
 
-                        // if tag already exists, increment popularity and return
-                        if (tagExists) {
-                            tagExists.popularity++;
-                            await tagExists.save();
+						// if tag already exists, increment popularity and return
+						if (tagExists) {
+							tagExists.popularity++;
+							await tagExists.save();
 
-                            return { ...tagExists._doc, new: false };
-                        }
+							return { ...tagExists._doc, new: false };
+						}
 
-                        const newTag = new Tags({ name: tagName }) as iTag;
-                        await newTag.save();
+						const newTag = new Tags({ name: tagName }) as iTag;
+						await newTag.save();
 
-                        return { ...newTag._doc, new: true }
-                    }),
-                );
+						return { ...newTag._doc, new: true };
+					}),
+				);
 
-                if (!createdTags.some((tag) => tag)) {
-                    throw Error('An error ocurred while creating the tags');
-                }
-            }
+				if (!createdTags.some((tag) => tag)) {
+					throw Error('An error ocurred while creating the tags');
+				}
+			}
 
-            const post = new Posts({ ...args, tags: createdTags }) as iPost;
-            await post.save();
+			const post = new Posts({ ...args, tags: createdTags }) as iPost;
+			await post.save();
 
-            return post;
-        }
-    },
-    ToggleLike: {
-        type: PostType,
-        description: "Toggles the like for a for of the authenticated user",
-        args: { postId: { type: GraphQLNonNull(GraphQLString) } },
-        resolve: async (_, args, ctx: Context) => {
-            if (ctx.req.cookies.error) throw Error(ctx.req.cookies.errorMessage)
+			return post;
+		},
+	},
+	ToggleLike: {
+		type: PostType,
+		description: 'Toggles the like for a for of the authenticated user',
+		args: { postId: { type: GraphQLNonNull(GraphQLString) } },
+		resolve: async (_, args, ctx: Context) => {
+			if (ctx.req.cookies.error) throw Error(ctx.req.cookies.errorMessage);
 
-            const { postId } = args;
-            const { _id: userId } = ctx.req.cookies.profile;
+			const { postId } = args;
+			const { _id: userId } = ctx.req.cookies.profile;
 
-            const post = (await Posts.findByIdAndUpdate(postId, {
-                $inc: { interactions: 1 },
-            })) as iPost;
+			const post = (await Posts.findByIdAndUpdate(postId, {
+				$inc: { interactions: 1 },
+			})) as iPost;
 
-            if (!post) throw Error('No post found with the given id.');
+			if (!post) throw Error('No post found with the given id.');
 
-            const likes = post.likes.map((user) => user.toString());
+			const likes = post.likes.map((user) => user.toString());
 
-            const update = likes.includes(userId) ? '$pull' : '$push';
-            const newPost = await Posts.findByIdAndUpdate(
-                post._id,
-                { [update]: { likes: userId } },
-                { new: true },
-            );
+			const update = likes.includes(userId) ? '$pull' : '$push';
+			const newPost = await Posts.findByIdAndUpdate(
+				post._id,
+				{ [update]: { likes: userId } },
+				{ new: true },
+			);
 
-            return newPost;
-        }
-    },
-    DeletePost: {
-        type: PostType,
-        description: "Delete a post from the authenticated user",
-        args: { postId: { type: GraphQLNonNull(GraphQLString) } },
-        resolve: async (_, args, ctx: Context) => {
-            if (ctx.req.cookies.error) throw Error(ctx.req.cookies.errorMessage)
+			return newPost;
+		},
+	},
+	DeletePost: {
+		type: PostType,
+		description: 'Delete a post from the authenticated user',
+		args: { postId: { type: GraphQLNonNull(GraphQLString) } },
+		resolve: async (_, args, ctx: Context) => {
+			if (ctx.req.cookies.error) throw Error(ctx.req.cookies.errorMessage);
 
-            const { postId } = args;
-            const post = (await Posts.findByIdAndDelete(postId)) as iPost;
+			const { postId } = args;
+			const post = (await Posts.findOneAndDelete({
+				_id: postId,
+				user: ctx.req.cookies.profile.id,
+			})) as iPost;
 
-            if (!post) throw Error('No posts found with the given id');
+			if (!post) throw Error('No posts found with the given id');
 
-            return post;
-        }
-    }
-})
+			return post;
+		},
+	},
+};
 
 export default Mutations;
--- server/routes/tags/validator.ts
+++ server/routes/tags/validator.ts
@@ -3,13 +3,13 @@ import Joi from '@hapi/joi';
 import regex from '../../helpers/regex';
 
 const validateTags = (tags: string[]) => {
-    const schema = Joi.object({
-        name: Joi.string().max(45).regex(regex.tag).required(),
-        popularity: Joi.number().min(0),
-    });
+	const schema = Joi.object({
+		name: Joi.string().max(45).regex(regex.tag).required(),
+		popularity: Joi.number().min(0),
+	});
 
-    const validTags = tags.map((tag) => schema.validate({ name: tag }).error);
-    return validTags.some((tagResult) => tagResult);
+	const validTags = tags.map((tag) => schema.validate({ name: tag }).error);
+	return validTags.some((tagResult) => tagResult);
 };
 
 export default validateTags;
